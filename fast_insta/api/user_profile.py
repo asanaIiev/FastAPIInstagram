1b5b675b91f22e71bce4b38b67b5889286ce0ec8
Ryui-code <ulanbektalasbaev2020@gmail.com>
2026-05-22 16:05:46 +0600

    Hi!

--- fast_insta/api/user_profile.py
+++ fast_insta/api/user_profile.py
@@ -1,11 +1,11 @@
-from fast_insta.database.models import UserProfile
 from fast_insta.database.db import SessionLocal
-from fastapi import APIRouter, HTTPException, Depends
+from fast_insta.database.models import UserProfile
 from fast_insta.database.schema import UserProfileInputSchema, UserProfileOutSchema
-from sqlalchemy.orm import Session
+from fastapi import HTTPException, APIRouter, Depends, status
 from typing import List
+from sqlalchemy.orm import Session
 
-users_router = APIRouter(prefix='/users')
+user_router = APIRouter(prefix='/users')
 
 async def get_db():
     db = SessionLocal()
@@ -14,45 +14,42 @@ async def get_db():
     finally:
         db.close()
 
-@users_router.post('/', response_model=UserProfileOutSchema, summary='Create user', tags=['Users'])
-async def create_user(user: UserProfileInputSchema, db: Session = Depends(get_db)):
-    if user: raise HTTPException(detail='This username or email already exists', status_code=404)
+@user_router.post('/', response_model=UserProfileOutSchema, tags=['Users'])
+async def post(user: UserProfileInputSchema, db: Session = Depends(get_db)):
+    if user: raise HTTPException(detail='This username or email already exists', status_code=400)
     user_db = UserProfile(**user.model_dump())
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
     return user_db
 
-@users_router.get('/', response_model=List[UserProfileOutSchema], summary='Get all users', tags=['Users'])
-async def users_list(db: Session = Depends(get_db)):
+@user_router.get('/', response_model=List[UserProfileOutSchema], summary='Get all users', tags=['Users'])
+async def get(db: Session = Depends(get_db)):
     users_db = db.query(UserProfile).all()
-    if not users_db:
-        raise HTTPException(detail='No users.', status_code=404)
+    if not users_db: raise HTTPException(detail='No users', status_code=status.HTTP_404_NOT_FOUND)
     return users_db
 
-@users_router.get('/{user_id}/', response_model=UserProfileOutSchema, summary='Get user by id', tags=['Users'])
-async def user_detail(user_id: int, db: Session = Depends(get_db)):
+@user_router.get('/{user_id}/', response_model=UserProfileOutSchema, summary='Get users by id', tags=['Users'])
+async def get(user_id: int, db: Session = Depends(get_db)):
     user_db = db.query(UserProfile).filter(UserProfile.id==user_id).first()
-    if not user_db:
-        raise HTTPException(detail='User not founded with this id.', status_code=404)
+    if not user_db: raise HTTPException(detail='No user', status_code=status.HTTP_404_NOT_FOUND)
     return user_db
 
-@users_router.put('/{user_id/}', response_model=dict, summary='Change user.', tags=['Users'])
-async def user_update(user_id: int, user: UserProfileInputSchema, db: Session = Depends(get_db)):
-    user_db1 = db.query(UserProfile).filter(UserProfile.id==user_id).first()
-    if not user_db1:
-        raise HTTPException(status_code=404, detail='User not founded by this id.')
-    for key, value in user.model_dump().items():
-        setattr(user_db1, key, value)
+@user_router.put('/{user_id}/', response_model=UserProfileOutSchema, summary='Change users credentials', tags=['Users'])
+async def put(user_id: int, user: UserProfileInputSchema, db: Session = Depends(get_db)):
+    user_db = db.query(UserProfile).filter(UserProfile.id==user_id).first()
+    if not user_db: raise HTTPException(detail='No user with this id', status_code=status.HTTP_404_NOT_FOUND)
+    update_user = user.model_dump(exclude_unset=True)
+    for key, value in update_user.items():
+        setattr(user_db, key, value)
     db.commit()
-    db.refresh(user_db1)
-    return {'detail': 'User has been changed.'}
+    db.refresh(user_db)
+    return user_db
 
-@users_router.delete('/{user_id}/', response_model=dict, summary='Delete user.', tags=['Users'])
-async def user_delete(user_id: int, db: Session = Depends(get_db)):
-    user_db2 = db.query(UserProfile).filter(UserProfile.id==user_id).first()
-    if not user_db2:
-        raise HTTPException(status_code=404, detail='User not founded by this id.')
-    db.delete(user_db2)
+@user_router.delete('/{user_id}/', response_model=dict, summary='Delete user', tags=['Users'])
+async def delete(user_id: int, db: Session = Depends(get_db)):
+    user_db = db.query(UserProfile).filter(UserProfile.id==user_id).first()
+    if not user_db: raise HTTPException(detail='No user with this id', status_code=status.HTTP_404_NOT_FOUND)
+    db.delete(user_db)
     db.commit()
-    return {'detail': 'User has been deleted.'}
+    return {'detail': 'User has been deleted'}
